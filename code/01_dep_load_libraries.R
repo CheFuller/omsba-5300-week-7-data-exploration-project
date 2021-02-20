2aa1b35bfe3b84f56806813dd366acbd80250602
Chequala Fuller <cfuller@seattleu.edu>
2021-02-20 08:47:42 -0800

    created code script and stored in the "code" folder

---
+++ code/01_dep_load_libraries.R
@@ -0,0 +1,8 @@
+##  Load libraries
+
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+library(vtable)
+library(jtools)
+library(estimatr)
---
+++ code/02_dep_read_data.R
@@ -0,0 +1,4 @@
+## Read in data
+
+list.files(path = "/data", pattern = "trends_up_to_")
+
