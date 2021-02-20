bfe898183891e57b0d8cd1785e312a10997f43f4
Chequala Fuller <cfuller@seattleu.edu>
2021-02-20 15:00:26 -0800

    expanded code in 02_dep_read_data script

--- code/02_dep_read_data.R
+++ code/02_dep_read_data.R
@@ -1,4 +1,23 @@
 ## Read in data
 
-list.files(path = "/data", pattern = "trends_up_to_")
+# read in Most Recent Cohorts Scorecard
+recent_scrcrd <- read_csv("../Lab3_Rawdata/Most+Recent+Cohorts+(Scorecard+Elements).csv")
 
+# read in ID Name Link and rename UNITID and OPEID to unitid and opeid, respectively.
+name_link <- read_csv("../Lab3_Rawdata/id_name_link.csv") %>%
+  rename(UNITID = unitid) %>%
+  rename(OPEID = opeid)%>%
+  distinct(schname, .keep_all = TRUE)
+
+# get the list of "trends_up_to" .csv files
+file_list <- list.files(path = "../Lab3_Rawdata/", pattern = "trends_up_to_", full.names = TRUE)
+
+process_file <- function(filename){
+  paste('../Lab3_Rawdata/', filename, sep = '')
+  
+}
+
+compile_data <- file_list %>%
+  map(read_csv) %>%
+  map(process_file)%>%
+  bind_rows()
