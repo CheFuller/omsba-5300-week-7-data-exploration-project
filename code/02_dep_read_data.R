89893674c2e66565a0d7a186f12e81a70c9dde38
Chequala Fuller <cfuller@seattleu.edu>
2021-02-21 08:09:34 -0800

    debugged and verified dataframes and function are outputting as expected

--- code/02_dep_read_data.R
+++ code/02_dep_read_data.R
@@ -1,16 +1,20 @@
 ## Read in data
 
 # read in Most Recent Cohorts Scorecard
-recent_scrcrd <- read_csv("../Lab3_Rawdata/Most+Recent+Cohorts+(Scorecard+Elements).csv")
+recent_cohorts <- read_csv("../Lab3_Rawdata/Most+Recent+Cohorts+(Scorecard+Elements).csv")
 
-# read in ID Name Link and rename UNITID and OPEID to unitid and opeid, respectively.
-name_link <- read_csv("../Lab3_Rawdata/id_name_link.csv") %>%
+# read in ID Name Link and rename UNITID, OPEID, SCHNAME from unitid, opeid, and schname, respectively.
+id_name_link <- read_csv("../Lab3_Rawdata/id_name_link.csv") %>%
   rename(UNITID = unitid) %>%
   rename(OPEID = opeid)%>%
-  distinct(schname, .keep_all = TRUE)
+  rename(SCHNAME = schname) %>%
+  distinct(SCHNAME, .keep_all = TRUE)
 
 # get the list of "trends_up_to" .csv files
-file_list <- list.files(path = "../Lab3_Rawdata/", pattern = "trends_up_to_", full.names = TRUE)
+file_list <- list.files(path = "../Lab3_Rawdata/", 
+                        pattern = "trends_up_to_", 
+                        full.names = TRUE)
+file_list # verify a list of 12 files
 
 process_file <- function(filename){
   paste('../Lab3_Rawdata/', filename, sep = '')
---
+++ code/03_dep_tidy_data.R
@@ -0,0 +1,7 @@
+## Load libraries
+source("../code/01_dep_load_libraries.R")
+
+# Read in data files
+source("../code/02_dep_read_data.R")
+
+# Perform tidy data
