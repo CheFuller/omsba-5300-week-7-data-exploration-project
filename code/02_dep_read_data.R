bc02b7ba11ed422de6acc9b377b5a4ac85faddea
Chequala Fuller <cfuller@seattleu.edu>
2021-02-21 09:23:05 -0800

    made more revisions to 02_dep_read_data.R and added to 03_dep_tidy_data.R

--- code/02_dep_read_data.R
+++ code/02_dep_read_data.R
@@ -1,13 +1,12 @@
 ## Read in data
 
 # read in Most Recent Cohorts Scorecard
-recent_cohorts <- read_csv("../Lab3_Rawdata/Most+Recent+Cohorts+(Scorecard+Elements).csv")
+recent_cohorts <- read_csv("../Lab3_Rawdata/Most+Recent+Cohorts+(Scorecard+Elements).csv") %>%
+  rename_with(toupper)                               # Capitalize all recent_cohorts variable names
 
 # read in ID Name Link and rename UNITID, OPEID, SCHNAME from unitid, opeid, and schname, respectively.
 id_name_link <- read_csv("../Lab3_Rawdata/id_name_link.csv") %>%
-  rename(UNITID = unitid) %>%
-  rename(OPEID = opeid)%>%
-  rename(SCHNAME = schname) %>%
+  rename_with(toupper) %>%                           # Capitalize all compile_data variable names
   distinct(SCHNAME, .keep_all = TRUE)
 
 # get the list of "trends_up_to" .csv files
@@ -17,11 +16,13 @@ file_list <- list.files(path = "../Lab3_Rawdata/",
 file_list # verify a list of 12 files
 
 process_file <- function(filename){
+  # this function reads in the list of "trends_up_to_" files
   paste('../Lab3_Rawdata/', filename, sep = '')
   
 }
 
 compile_data <- file_list %>%
-  map(process_file)%>%
-  map(read_csv) %>%
-  bind_rows()
+  map(process_file)%>%                              # process "trends_up_to_" files
+  map(read_csv) %>%                                 # read in file contents
+  bind_rows() %>%
+  rename_with(toupper)                              # Capitalize all compile_data variable names
---
+++ code/04_dep_data_analysis.R
@@ -0,0 +1,11 @@
+## Load libraries
+source("../code/01_dep_load_libraries.R")
+
+## Read in data files
+source("../code/02_dep_read_data.R")
+
+## Load tidy date
+source("../code/04_dep_tidy_data")
+
+## Perform regressions and modeling
+
