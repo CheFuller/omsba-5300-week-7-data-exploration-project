28fe6b0bc17bc55c4b75967921b9c42f48f25f33
Chequala Fuller <cfuller@seattleu.edu>
2021-02-21 10:56:58 -0800

    created new dataframes and merged data in 03_dep_tidy_data

--- code/03_dep_tidy_data.R
+++ code/03_dep_tidy_data.R
@@ -1,7 +1,22 @@
 ## Load libraries
 source("../code/01_dep_load_libraries.R")
 
-# Read in data files
+## Read in data files
 source("../code/02_dep_read_data.R")
 
-# Perform tidy data
+## Start performing tidy data
+
+# First, we'll create a new dataframe for the id name scorecards and call it 
+# scorecd_id_name. We'll accomplish this by merging the recent_cohorts and 
+# id_name_link dataframes together by both unit ID (UNITID) and ope ID (OPEID).
+
+scorecd_id_name <- merge(x = id_name_link, y = recent_cohorts,
+                        by = c("UNITID", "OPEID"), all.x = TRUE)
+
+# Next, create another data frame for the college scorecard through merging 
+# the scorecd_id_name dataframe with our compile_data dataframe by 
+# school name (SCHNAME). This will give us a more comprehensive spreadsheet 
+# to work with.
+
+coll_scorecd <- merge(x = scorecd_id_name, y = compile_data,
+                      by = "SCHNAME", all.x = TRUE)
